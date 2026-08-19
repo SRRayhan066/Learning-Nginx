6dda2776693e1e901a0a1d540ea548411005368d
SRRayhan066 <shafikulrahman66@gmail.com>
2026-08-19 22:14:19 +0600

    Implement docker-compose for multiple instance running

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from "express";
 
 const app = express();
 
+const App_Name = process.env.APP_NAME;
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello, World!</h1>");
+  res.send(`<h1>Hello, ${App_Name}!</h1>`);
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log(`${App_Name} is running on port 3000`);
 });
